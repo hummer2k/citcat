b9040136d08fef3587d76bc98747ef52be409101
Cornelius Adams <cornelius.adams@conlabz.de>
2020-05-24 15:04:07 +0200

    reindex bash

--- collect.sh
+++ collect.sh
@@ -2,9 +2,12 @@
 cd "$(dirname "$0")"
 docker-compose up -d
 
-if [ "$1" = "timeline" ]
+if [[ "$1" = "timeline" ]]
 then
     docker-compose exec -T php bin/console twitter:collect:timeline -q
+elif [[ "$1" = "reindex" ]]
+then
+    docker-compose exec -T php bin/console twitter:tweets:reindex --from-date=-2hours
 else
     docker-compose exec -T php bin/console twitter:collect:search --result_type=mixed
     docker-compose exec -T php bin/console twitter:collect:search --result_type=recent
