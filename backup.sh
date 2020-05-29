08ba6290891c4949674be5273e4d996538054b4e
Cornelius Adams <cornelius.adams@conlabz.de>
2020-05-29 10:45:54 +0200

    backup.sh

--- backup.sh
+++ backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+source .env
 docker-compose up -d
 
-now=$(date +"%Y-%m-%d_%T");
+now=$(date +"%Y-%m-%d_%H-%M-%S");
 mkdir -p var/backup
-docker-compose exec db mysqldump -u root -proot --no-create-info --insert-ignore twitter > var/backup/twitter-$now.sql
-gzip twitter-$now.sql
-
+docker-compose exec db mysqldump -u root -p$DB_ROOT_PASSWORD --no-create-info --insert-ignore twitter > var/backup/twitter-$now.sql
+gzip var/backup/twitter-$now.sql
