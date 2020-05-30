d21cef5e4aa40fbb6770c8a8b7b39f0680a2c7d3
Cornelius Adams <cornelius.adams@conlabz.de>
2020-05-30 15:19:40 +0200

    disable tty

--- backup.sh
+++ backup.sh
@@ -4,5 +4,5 @@ source .env
 
 now=$(date +"%Y-%m-%d_%H-%M-%S");
 mkdir -p var/backup
-docker-compose exec db mysqldump -u root -p$DB_ROOT_PASSWORD --no-create-info --insert-ignore twitter > var/backup/twitter-$now.sql
+docker-compose exec -T db mysqldump -u root -p$DB_ROOT_PASSWORD --no-create-info --insert-ignore twitter > var/backup/twitter-$now.sql
 gzip var/backup/twitter-$now.sql
