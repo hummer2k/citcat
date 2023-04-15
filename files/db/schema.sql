0f3cc75167f7c456bcbd68bbed67d7796275765c
Cornelius Adams <ca@conlabz.de>
2023-04-15 13:22:20 +0200

    Fix composer home dir, add services to setup.sh

--- files/db/schema.sql
+++ files/db/schema.sql
@@ -1,34 +1,19 @@
--- MariaDB dump 10.19  Distrib 10.5.18-MariaDB, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.19  Distrib 10.2.44-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Host: db    Database: twitter
+-- Host: localhost    Database: twitter
 -- ------------------------------------------------------
--- Server version	10.2.44-MariaDB-1:10.2.44+maria~bionic-log
+-- Server version	10.2.44-MariaDB-1:10.2.44+maria~bionic
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `api_requests`
---
-
-DROP TABLE IF EXISTS `api_requests`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `api_requests` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `url` varchar(2000) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `data` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `categories`
@@ -44,8 +29,18 @@ CREATE TABLE `categories` (
   PRIMARY KEY (`id`),
   KEY `IDX_3AF34668796A8F92` (`parent_category_id`),
   CONSTRAINT `FK_3AF34668796A8F92` FOREIGN KEY (`parent_category_id`) REFERENCES `categories` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (47,'Dataset 1',NULL),(48,'Dataset 2',NULL),(49,'Dataset 1.1',47),(50,'Dataset 1.2',47),(51,'Dataset 2.1',48),(52,'Dataset 2.2',48);
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `friends`
@@ -66,6 +61,15 @@ CREATE TABLE `friends` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `friends`
+--
+
+LOCK TABLES `friends` WRITE;
+/*!40000 ALTER TABLE `friends` DISABLE KEYS */;
+/*!40000 ALTER TABLE `friends` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tweet_category`
 --
 
@@ -82,6 +86,15 @@ CREATE TABLE `tweet_category` (
   CONSTRAINT `FK_45AA73CA12469DE2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tweet_category`
+--
+
+LOCK TABLES `tweet_category` WRITE;
+/*!40000 ALTER TABLE `tweet_category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tweet_category` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tweets`
@@ -110,19 +123,13 @@ CREATE TABLE `tweets` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `users`
+-- Dumping data for table `tweets`
 --
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(255) NOT NULL,
-  `screen_name` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=183 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `tweets` WRITE;
+/*!40000 ALTER TABLE `tweets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tweets` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -133,4 +140,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-15  9:31:38
+-- Dump completed on 2023-04-15 11:19:11
--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-docker network create dockerdev-proxy
-ln -s docker-compose.dev.yml docker-compose.override.yml
+ln -sf docker-compose.dev.yml docker-compose.override.yml
 docker-compose up -d
 docker-compose exec php bin/composer install
+
+echo "= Services ======================================================"
+echo ""
+echo "  Web-Interface: http://localhost:8005"
+echo "  Kibana:        http://localhost:5601 (may take a while to load)"
+echo "  phpMyAdmin:    http://localhost:8040"
+echo ""
+echo "================================================================="
+echo ""
